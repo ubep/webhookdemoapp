69548ce4ba02b205d8e406b71190622f031c0798
Ulf B <u.biallas@epages.com>
2016-08-22 11:29:38 +0200

    add dashboard

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var express = require('express');
+var express = require('express')
+var app = express()
 var bodyParser = require('body-parser')
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 
-var app = express();
+var sockets = []
+
 var jsonParser = bodyParser.json()
 var notifications = []
 
 app.get('/notifications', function(req, res){
-  res.send(JSON.stringify(notifications));
+    res.send(JSON.stringify(notifications))
 });
 
 app.post('/notifications', jsonParser, function(req, res){
-  notification = req.body
-  notifications.push(notification)
-  return res.sendStatus(200)
+    notification = req.body
+    notifications.push(notification)
+    for(var i=0; i<sockets.length; ++i) {
+        sockets[i].emit('event')
+    }
+    return res.sendStatus(200)
 });
 
-app.listen(3000);
+app.use('/dashboard', express.static(__dirname + '/html'))
+
+io.on('connection', function (socket) {
+    sockets.push(socket)
+})
+
+server.listen(3000)
